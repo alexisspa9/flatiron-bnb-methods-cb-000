0ee7bbbcf5a2c505b5473cbb5439d471e8990872
Alexander Spanos <alexisspa9@gmail.com>
2018-04-10 08:49:01 +0000

    automatically backed up by learn

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -22,7 +22,7 @@ describe Neighborhood do
       it 'knows the neighborhood with the highest ratio of reservations to listings' do
         expect(Neighborhood.highest_ratio_res_to_listings).to eq(@nabe1)
       end
-      
+
     end
 
     describe ".most_res" do
